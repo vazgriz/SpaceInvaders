6550e77b689883ad2a352871e692c8cc5cd58d02
Unknown <vazgriz@gmail.com>
2018-01-18 20:32:34 -0600

    Add LoadFiles to Utilities

--- SpaceInvaders/Utilities.h
+++ SpaceInvaders/Utilities.h
@@ -1,8 +1,12 @@
 #include <vulkan/vulkan.h>
 #include <stdexcept>
+#include <vector>
+#include <fstream>
 
 #define VK_CHECK(exp, msg)                         \
 {                                                  \
 	VkResult result = exp;                         \
 	if (result < 0) throw std::runtime_error(msg); \
 }
+
+std::vector<char> LoadFile(const std::string& fileName);
